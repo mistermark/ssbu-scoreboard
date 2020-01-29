5a6dc41420e8ee90097cd993b4203ace13fb9f5a
Mark de Jong <mark@markdejong.com>
2020-01-29 14:30:11 +0100

    Extracting API service

--- api/routes/upload.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const upload = require("../controller/upload.controller");
-
-// Upload
-router.route("/").post(upload.upload);
-
-module.exports = router;
